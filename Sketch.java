231aaa203e771c783741252878f6684eae8a1dfe
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-03-25 13:08:55 -0400

    Sketch.java

--- Sketch.java
+++ Sketch.java
@@ -162,7 +162,19 @@ public class Sketch extends PApplet {
    * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
    */
   public void draw_section5() {
+    int intX = 0;
+    int intY = 0;
 
+    for (int intRow = 0; intRow < 30; intRow++) {
+      for (int intColumn = 0; intColumn < 30; intColumn++) {
+        intX = 3 + intRow * 10;  // Instead of zero, calculate the proper intX location using 'intRow'
+        intY = 0 + 3 + intColumn * 10; // Instead of zero, calculate the proper intY location using 'intColumn'
+
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      }
+    }
   }
 
   public void draw_section6() {
